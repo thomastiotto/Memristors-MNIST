19916f2df7f48af40989eb775c9213bd49ccc569
Thomas <thomas.tiotto@outlook.com>
2021-02-09 15:34:44 +0100

    - Added accuracy calculation to mOja_test.py

--- mOja_test.py
+++ mOja_test.py
@@ -4,19 +4,26 @@ from extras import *
 from learning_rules import mOja, mPES
 from neurons import AdaptiveLIFLateralInhibition
 
-seed = 42
+seed = None
+np.random.seed( seed )
 beta = 1
+neurons = 20
+time = 10
+
+train_neurons = np.random.randint( 2, size=neurons )
+train_neurons[ train_neurons == 0 ] = -1
+train_neurons *= 2
 
 model = nengo.Network( seed=seed )
 with model:
-    inp = nengo.Node( [ 2, 2, -2, -2 ] )
-    pre = nengo.Ensemble( 4, 1,
+    inp = nengo.Node( lambda t, x: train_neurons if t < time / 2 else [ -2 ] * neurons, size_in=1 )
+    pre = nengo.Ensemble( neurons, 1,
                           # neuron_type=AdaptiveLIFLateralInhibition(),
                           seed=seed )
     nengo.Connection( inp, pre.neurons, seed=seed )
     conn = nengo.Connection( pre.neurons, pre.neurons,
                              # learning_rule_type=nengo.learning_rules.Oja( beta=beta ),
-                             learning_rule_type=mOja( beta=beta, noisy=0.01 ),
+                             learning_rule_type=mOja( gain=1e6, beta=beta, noisy=0 ),
                              # transform=np.random.random( (pre.n_neurons, pre.n_neurons) ),
                              transform=np.zeros( (pre.n_neurons, pre.n_neurons) ),
                              # transform=np.eye( pre.n_neurons ),
@@ -31,13 +38,21 @@ with model:
     weight_probe = nengo.Probe( conn, "weights" )
 
 with nengo.Simulator( model, seed=seed ) as sim:
-    sim.run( 10 )
+    sim.run( time )
 
 fig1 = neural_activity_plot( sim.data[ pre_probe ], sim.trange() )
 fig1.show()
-plt.set_cmap( 'jet' )
+plt.set_cmap( "jet" )
 fig2, ax = plt.subplots( 1 )
 ax.matshow( sim.data[ weight_probe ][ -1 ] )
 fig2.show()
 print()
 print( sim.data[ weight_probe ][ -1 ] )
+
+avg_act = np.mean( sim.data[ pre_probe ][ int( time / sim.dt / 2 ): ], axis=0 )
+count = neurons
+for i, (neur, act) in enumerate( zip( train_neurons, avg_act ) ):
+    if neur == -2 and act > 100 or neur == 2 and act < 100:
+        count -= 1
+        print( f"Neuron {i}: {neur}->{act}" )
+print( f"Accuracy: {(count / neurons) * 100} %" )
